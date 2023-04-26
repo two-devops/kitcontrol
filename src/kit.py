7fa49a4be4ca2463e7cfa43bc3335804baaeedde
miquelangel-castillo <miquelangel.castillo@gmail.com>
2023-04-27 00:41:55 +0200

    use pytest fixture

--- src/kit.py
+++ src/kit.py
@@ -11,18 +11,18 @@ class Kit:
         self.name = name
 
         # Load kit config
-        self.config = self.load_config()
+        self.config = self._load_config()
 
         # Merge values
         self.data = merge(self.config['data'], values)
 
         # Load template 
-        self.template = self.load_template()
+        self.template = self._load_template()
 
-    def load_config(self):
+    def _load_config(self):
         return yaml.load(open(f"{BASEPATH}/{self.name}/{self.name}.yaml", "r"), Loader=yaml.loader.SafeLoader)
 
-    def load_template(self):
+    def _load_template(self):
         env = Environment(loader=FileSystemLoader(f"{BASEPATH}/{self.name}/"))
         return env.get_template(self.config['template'])
 
