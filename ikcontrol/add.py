7f42becc628ffd4d9be57779ae152acb7a950872
david.moya <david.moya@invisiblebits.com>
2023-05-01 18:38:20 +0200

    refactor test and ikcontrol

--- ikcontrol/add.py
+++ ikcontrol/add.py
@@ -1,6 +1,17 @@
 class AddResources:
     """
     class to add news resources, kits, targets and pipelines
+
+    Methods:
+    -------
+    add_kit()
     """
 
-    # def add_kit(self):
+    def add_kit(self, entity_name, name):
+        """Add kit
+
+        Args:
+            name (_type_): _description_
+            entity_name (_type_): _description_
+        """
+        print(entity_name, name)
--- ikcontrol/cli.py
+++ ikcontrol/cli.py
@@ -3,6 +3,14 @@ import click
 from init import InitApp
 from add import AddResources
 
+ADD = [
+    "kit",
+    "target",
+    "pipeline"
+]
+
+CONFIG_NAME = "kitcontrol.yaml"
+
 @click.group(name='ikctl')
 def ikctl():
     '''
@@ -18,19 +26,21 @@ def init(path):
     '''
     initapp = InitApp(path)
     initapp.build_folders()
-    initapp.create_config_files("kitcontrol.yaml")
+    initapp.create_config_files(CONFIG_NAME)
 
 
 @click.command(name="add")
-@click.option('-k', '--kit', default=None, help='kit name that we are installing')
-@click.option('-t', '--target', default=None, help='destination name where we are going to install the kit')
-@click.option('-p', '--pipeline', default=None, help='params to adding at command')
-def add(kit, target, pipeline):
+# @click.option('-k', '--kit', default=None, help='kit name that we are installing')
+# @click.option('-t', '--target', default=None, help='destination name where we are going to install the kit')
+# @click.option('-p', '--pipeline', default=None, help='params to adding at command')
+@click.argument('entity_name', type=click.Choice(ADD))
+@click.argument('name')
+def add(entity_name, name):
     '''
     command to adding entities: kits, targets or pipelines
     '''
-    add = AddResources()
-    click.echo(kit)
+    added = AddResources()
+    added.add_kit(entity_name, name)
 
 ikctl.add_command(init)
 ikctl.add_command(add)
