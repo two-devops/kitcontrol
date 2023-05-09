e16c48a411b3ae90ced3b2d9e892cbe1165cd8d0
david.moya <david.moya@invisiblebits.com>
2023-05-09 22:13:25 +0200

    refacto test add

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,30 +1,19 @@
 from add import AddResources
 
 class TestAddResources:
+    """testing class AddResource"""
 
-    """
-    testing class AddResource
-    """
     entities = ["kit", "target", "pipeline"]
 
-    def test_add_kit(self):
-        """
-        testing add_kit function
-        """
+    def test_entities(self):
+        """testing entities function"""
+
         addresource = AddResources()
 
         for entity in self.entities:
             if entity == "kit":
                 assert True is addresource.add_entity(entity, "test")
-
-    def test_add_target(self):
-        """
-        testing add_target function
-        """
-        pass
-
-    def test_add_pipeline(self):
-        """
-        testing add_pipeline function
-        """
-        pass
+            if entity == "target":
+                assert True is addresource.add_entity(entity, "test")
+            if entity == "pipeline":
+                assert True is addresource.add_entity(entity, "test")
