3dc41d1d8418a0addf135a2fdcf56249b3a706f7
david.moya <david.moya@invisiblebits.com>
2023-05-20 21:43:37 +0200

    add system mkls

--- kitcontrol/cmds/system.py
+++ kitcontrol/cmds/system.py
@@ -1,16 +1,25 @@
-from subprocess import run
+from subprocess import run, PIPE
 from os.path import exists
 
 import yaml as yml
 
 class System:
     """Manage to create folders and files"""
-    
 
     def mkdir(self, path):
         """ Create fields"""
-        return run(["mkdir", "-p", path], capture_output=True, text=True, check=True, timeout=30)
+        try:
+            return run(["mkdir", "-p", path], capture_output=True, text=True, check=True, timeout=30)
+        except FileExistsError as err:
+            print(err)
 
+    def mkls(self, entity):
+        """ Show fields and files"""
+        try:
+            process = run(["ls", "-1", entity], check=True, stdout=PIPE, universal_newlines=True, timeout=30)
+            return process.stdout
+        except FileNotFoundError as err:
+            print(err)
 
     def mkfile(self, path, filename, data=None):
         """ Create files"""
@@ -23,7 +32,6 @@ class System:
                 file.write(config_file)
         except Exception as error:
             print(f"Error: {error}")
-
 
     def search(self, value):
         """Search field and file"""
