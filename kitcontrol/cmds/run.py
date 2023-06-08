0d549795b677475824c4f0fd522c5aa8de77d872
miquelangel-castillo <miquelangel.castillo@gmail.com>
2023-06-09 01:07:32 +0200

    pipeline first approach

--- kitcontrol/cmds/run.py
+++ kitcontrol/cmds/run.py
@@ -7,7 +7,8 @@ from core.pipeline import Pipeline
 class Run:
     """Class to exec kits, targets and pipelines"""
 
-    def __init__(self, kit, target, pipeline=None, sudo=None):
+
+    def __init__(self, kit=None, target=None, pipeline=None, sudo=None, system=System()):
         self.kit = kit
         self.target = target
         self.pipeline = pipeline
@@ -15,10 +16,19 @@ class Run:
         self.config = Config()
         self.check = Checks()
         self.config.check_config()
-        self.check.check_if_not_exist(self.config.kits_dir + "/" + self.kit, "not found")
-        self.check.check_if_not_exist(self.config.targets_dir + "/" + self.target + ".yaml", "not found")
+
+        if self.kit:
+            self.check.check_if_not_exist(self.config.kits_dir + "/" + self.kit, "not found")
+        if self.target:
+            self.check.check_if_not_exist(self.config.targets_dir + "/" + self.target + ".yaml", "not found")
+
 
     def run(self):
         """exec kits"""
-        pipeline = Pipeline(kit=self.kit, target=self.target)
-        pipeline.run()
+        if self.pipeline:
+            pipeline = Pipeline(self.pipeline)
+            pipeline.start()
+        else: 
+            pipeline = Pipeline(kit=self.kit, target=self.target)
+            pipeline.run()
+
